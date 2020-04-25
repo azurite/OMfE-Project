360d4c6e33e06b9ceb69dd33db80ebbac83a99bd
Neville Walo <walon@student.ethz.ch>
2020-04-25 14:20:31 +0200

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 #include "korali.hpp"
 #include "model/model.hpp"
-#inlcude "src/config.h"
+#include "model/src/config.h"
 
 int main(int argc, char **argv) {
 
@@ -11,39 +11,39 @@ int main(int argc, char **argv) {
 
     e["Problem"]["Type"] = "Optimization/Stochastic";
     e["Problem"]["Objective"] = "Minimize";
-    e["Problem"]["Objective Function"] = &direct;
+    e["Problem"]["Objective Function"] = &fitness;
 
 
 // Defining the problem's variables.
     e["Variables"][0]["Name"] = "THREADBLOCK_TILE_M";
     e["Variables"][0]["Lower Bound"] = 0.0;
     e["Variables"][0]["Upper Bound"] = M;
-    e["Variables"][0]["Granularity"] = 1.0
+    e["Variables"][0]["Granularity"] = 1.0;
 
     e["Variables"][1]["Name"] = "THREADBLOCK_TILE_N";
     e["Variables"][1]["Lower Bound"] = 0.0;
     e["Variables"][1]["Upper Bound"] = N;
-    e["Variables"][1]["Granularity"] = 1.0
+    e["Variables"][1]["Granularity"] = 1.0;
 
     e["Variables"][2]["Name"] = "THREADBLOCK_WARP_TILE_K";
     e["Variables"][2]["Lower Bound"] = 0.0;
     e["Variables"][2]["Upper Bound"] = K;
-    e["Variables"][2]["Granularity"] = 1.0
+    e["Variables"][2]["Granularity"] = 1.0;
 
     e["Variables"][3]["Name"] = "WARP_TILE_M";
     e["Variables"][3]["Lower Bound"] = 0.0;
     e["Variables"][3]["Upper Bound"] = M;
-    e["Variables"][3]["Granularity"] = 1.0
+    e["Variables"][3]["Granularity"] = 1.0;
 
     e["Variables"][4]["Name"] = "WARP_TILE_N";
     e["Variables"][4]["Lower Bound"] = 0.0;
     e["Variables"][4]["Upper Bound"] = N;
-    e["Variables"][4]["Granularity"] = 1.0
+    e["Variables"][4]["Granularity"] = 1.0;
 
     e["Variables"][5]["Name"] = "SPLIT_K";
     e["Variables"][5]["Lower Bound"] = 0.0;
     e["Variables"][5]["Upper Bound"] = K;
-    e["Variables"][5]["Granularity"] = 1.0
+    e["Variables"][5]["Granularity"] = 1.0;
 
     // TODO Decide which solver to use
     // Configuring CMA-ES parameters
@@ -53,9 +53,8 @@ int main(int argc, char **argv) {
     e["Solver"]["Termination Criteria"]["Max Generations"] = 100;
 
     auto k = korali::Engine();
-    std::cout << "Engine and Experiment loaded" << std::endl;
-    std::cout << "fitness(3.14) = " << fitness(3.14) << std::endl;
 
-    //k.run(e);
+
+    k.run(e);
     return 0;
 }
